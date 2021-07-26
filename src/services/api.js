1b174b48ab14d2285db712f6974efcf15add0e81
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2021-07-26 13:27:25 -0300

    Validate package dependecies

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333/'
+    baseURL: 'https://apprender-backend.herokuapp.com/'
 });
 // 'https://apprender-backend.herokuapp.com/'
 export default api;
